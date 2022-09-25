2191738e1b1b5c7fb8361759d299799fac190504
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-09-24 21:44:58 -0500

    for completar iteracion contraseñade yeil con srt

--- python/apis/yield.py
+++ python/apis/yield.py
@@ -1,24 +1,35 @@
 #"//how   make  a funtion  with   python?"?
+try:
 
-def saludo():
-    yield "saludo"
-    yield "hola"
-    yield "qué tal"
-    yield "Alberto"
+    def saludo():
+        yield "hi"
+        yield "how are you"
+        yield " you ok?"
+        yield "nice"
+    for i in saludo():
+        print (i)
+        
 
-correct_paswords= (3,5,6)
+
+    correct_paswords= (3,5,6)
+        
+    def generate_numbers():
+        for d1 in range(10):
+                for d2 in range(10):
+                        for d3 in range(10):
+                            yield (d1,d2,d3)
+                            
+                            
+    for(a,b,c) in generate_numbers():
+        if(a,b,c) == correct_paswords:
+            print ("Is correct your password ")
+            print(a,b,c)
+            break
+        
+
+        
     
-def generate_numbers():
-    for d1 in range(10):
-            for d2 in range(10):
-                    for d3 in range(10):
-                        yield (d1,d2,d3)
-                        
-                        
-for(a,b,c) in generate_numbers():
-    if(a,b,c) == correct_paswords:
-        print ("Is correct your password ")
-        print(a,b,c)
-        break
-
-
+        
+except:
+    print("Saludo is not implemented")
+    
