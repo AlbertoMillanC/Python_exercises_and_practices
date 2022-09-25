189008787c5d7bf8d26af8e269e02450083234f6
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-09-24 20:46:33 -0500

    for completar iteracion contraseña

--- python/apis/yield.py
+++ python/apis/yield.py
@@ -5,11 +5,18 @@ def saludo():
     yield "hola"
     yield "qué tal"
     yield "Alberto"
+
+correct_paswords= (356)
     
 def generate_numbers():
     for d1 in range(10):
             for d2 in range(10):
                     for d3 in range(10):
                         yield (d1,d2,d3)
+for(a,b,c) in generate_numbers():
+    if(a,b,c) == correct_paswords:
+        print ("Is correct your password ")
+        print(a,b,c)
+        break
 
 
