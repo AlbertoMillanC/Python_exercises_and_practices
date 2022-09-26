34165366293c36ee91567c40eaecc1311449b175
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-09-25 23:15:12 -0500

    arreglando bug de digitación

--- python/apis/prueba_de_primabilidad.py
+++ python/apis/prueba_de_primabilidad.py
@@ -1,21 +1,16 @@
 #los multiplos de 2, 3, 5, 7 no son primos. el restante si lo es. de
 
 
-correct_passworks = (17)
+correct_password = 17
 
 def primos():
     n= int(input("input your numbers "))
-    for i in (range(0,120,2)):      
-        for j in (range(0,120,3)):
-            for k in (range(0,120,5)):
-                for l in (range(0,120,7)):
-                    yield 
+    if i in range(n):
+        i == correct_password
+if __name__ == "__main__":
+    primos()
                     
-for(i,j,k,l) in primos():
-        if(i,j,k,l) == correct_passworks:
-            print ("Is correct your password ")
-            print(i,j,k,l)
-            continue
+
                     
                     
 
