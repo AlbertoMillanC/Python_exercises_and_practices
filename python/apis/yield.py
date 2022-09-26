b990004d57ebc2e850315a0827a3212fed539d0f
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-09-25 22:14:05 -0500

    prueba de primabilidad con yieldn

--- python/apis/yield.py
+++ python/apis/yield.py
@@ -11,21 +11,20 @@ try:
         
 
 
-    correct_paswords= (3,5,6)
+    correct_paswords= (3,1,6)
         
     def generate_numbers():
         for d1 in range(10):
                 for d2 in range(10):
                         for d3 in range(10):
-                            print (d1,d2,d3)
+                            yield (d1,d2,d3)
                             
                             
     for(a,b,c) in generate_numbers():
         if(a,b,c) == correct_paswords:
             print ("Is correct your password ")
             print(a,b,c)
-            break
-        
+            continue
 
         
     
