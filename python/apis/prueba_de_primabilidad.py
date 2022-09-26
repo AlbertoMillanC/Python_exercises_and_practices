c0e53d8bbb8ee583c4c2504c4c2e3e906402eb08
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-09-26 13:46:48 -0500

    una funcion de primos ok

--- python/apis/prueba_de_primabilidad.py
+++ python/apis/prueba_de_primabilidad.py
@@ -1,16 +1,31 @@
+
 #los multiplos de 2, 3, 5, 7 no son primos. el restante si lo es. de
-
-
-correct_password = 17
-
-def primos():
-    n= int(input("input your numbers "))
-    if i in range(n):
-        i == correct_password
-if __name__ == "__main__":
-    primos()
-                    
-
+try:
+    i = 1
+      
+    a= int (input("input your number "))
+    b = a % 2 and a % 3 and a % 5 and a % 7
+   
+    
+    def primos():
+        if b != 0:
+            print("Primos")                      
+        else:
+            print("Primos incorrect")
+            
+    
+    
+                
+        
+    if __name__ == "__main__":
+        primos()
+        
+    print(b)
+     
+                  
+except:
+    print("Failed to interrupt")
+    
                     
                     
 
