a2a626335fce296dc86490b956fcdb637c898517
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-09-29 17:58:13 -0500

    dando formato al documento while

--- python/apis/while_3.py
+++ python/apis/while_3.py
@@ -1,22 +1,19 @@
 def new_func(n):
-    i=0
-    a=0
-    while(i<n):
+    i = 0
+    a = 0
+    while (i < n):
         a = a+i
         i = i+2
     return a
-    
-print (new_func(5))
 
-    
-   
+
+print(new_func(5))
 
 
 try:
 
     new_func()
-    
-   
-except :
+
+
+except:
     print("Hubo un error. Saliendo del programa")
-   
