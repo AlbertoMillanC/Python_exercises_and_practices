d617b1c544948e6c18d9b828e9a5c2c7369ca3a6
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-09-28 22:59:43 -0500

    generando py en listas paraprimabilidad

---
+++ python/apis/primabilidad _en_listas.py
@@ -0,0 +1,22 @@
+import random
+import timeit
+
+try:
+
+    a = (random.randint(2, 100000000))
+    b = a % 2 and a % 3 and a % 5 and a % 7
+
+    def primos():
+        if b != 0:
+            print(f"El número {a} SI un número primo")
+
+        else:
+            print(f"El número {a} NO es Primo")
+
+    if __name__ == "__main__":
+        primos()
+
+
+except Exception as e:
+    print(e)
+             
