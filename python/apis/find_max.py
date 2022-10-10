74ed6601baaa167a5289c45cb40cd63e8c9b7ae6
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-10-09 22:33:21 -0500

    cambios en def para hacer max con float.

--- python/apis/find_max.py
+++ python/apis/find_max.py
@@ -1,15 +1,13 @@
 #funcion para encotrar un numero maximo de una lista
-try:
-        def find_max(nums):
-                max_num = float("-inf")
-                for i in nums:
-                        if i > max_num:
-                                max_num = i                                
-                return max_num
 
+def find_max(nums):
+        max_num = float("-inf")
+        for i in nums:
+                if i > max_num:
+                        max_num = i                                 
+        return max_num
 
-        __name__ == "__main__"  
-        find_max(nums = [2.8,3.53,4.5,78.8,12.8,1.8,2.7,555.5])
-except Exception as e:
-        print("404")
-        print(e)
+nums = 2.8,3.53,4.5,78.8,12.8,1.8,2.7,555.5
+
+__name__ == "__main__"  
+find_max(nums)
