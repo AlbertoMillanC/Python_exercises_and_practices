623e823804c97d39c8bc3b2bdf8ee1eb8e61539d
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-10-12 11:38:06 -0500

    metodo __del__ uso y practica

---
+++ python/08_poo/destructor_.py
@@ -0,0 +1,24 @@
+from msilib.schema import Class
+
+
+try:
+
+    class Animales:
+
+        def __init__(self, name, age):
+            self.name = name
+            self.age = age
+
+        def grupo(self):
+            print("Nombre del grupo", self.name)
+
+        def __del__(self):
+            print("Se ha eliminado el objeto")
+            
+    animal = Animales("aves")
+    animal.grupo()    
+    del animal
+    print(animal.name)
+
+except Exception:
+    print("Error")
