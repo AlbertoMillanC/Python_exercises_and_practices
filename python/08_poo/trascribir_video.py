60a25c6dd04f763da1f5dd893ff15ab5f7d38b1e
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-10-15 21:24:24 -0500

    whisper add pip install

--- python/08_poo/trascribir_video.py
+++ python/08_poo/trascribir_video.py
@@ -1,14 +1,38 @@
+import logging
 import pytube
 import whisper
+import sys
+import argparse
 
-youtube_url = 'https://www.youtube.com/watch?v=J-hjMWej4f0'
+parser = argparse.ArgumentParser(description='Transcript a YouTube video using Whisper')
 
-youtube_video = pytube.YouTube(youtube_url)
+parser.add_argument("--video", help = "Pass the YouTube url to transcribe")
+parser.add_argument("--model", help = "Indicate the Whisper model to download", default="small")
+args = parser.parse_args()
 
-audio = youtube_video.streams.get_audio_only()
-audio.download(file_name='tmp.mp4')
+logging.basicConfig(
+  level=logging.INFO,
+  format="%(asctime)s [%(levelname)s] %(message)s",
+  handlers=[
+    logging.StreamHandler(sys.stdout)
+  ]
+)
 
-model = whisper.load_model('small')
+if not args.video:
+  logging.error("Please pass a YouTube url to transcribe")
+  exit()
+
+logging.info("Downloading Whisper model")
+model = whisper.load_model(args.model)
+
+logging.info("Downloading the video from YouTube...")
+youtubeVideo = pytube.YouTube(args.video)
+
+logging.info("Get only the audio from the video")
+audio = youtubeVideo.streams.get_audio_only()
+audio.download(filename='tmp.mp4')
+
+logging.info("Transcribe the audio")
 result = model.transcribe('tmp.mp4')
 
-print (result["text"])
+print(result["text"])
