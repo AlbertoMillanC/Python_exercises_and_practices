96204d0ae5f07b8aee9de45e53bf56f64389ffb1
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-11-08 21:13:53 -0500

    selenium add file

---
+++ api_and_practices/pygame_all/selenimun_.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+
+# This call to setup() does all the work
+setup(
+    name="Selenium-Screenshot",
+    version="2.0.0",
+    description="This package is used to Clipped Images of Html Elements of Selenium Webdriver",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PyWizards/Selenium_Screenshot",
+    author="PyWizard org",
+    author_email="py.wizard.org@gmail.com",
+    license="MIT",
+    platforms="any",
+    python_requires=">=3.6.1",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    include_package_data=True,
+    install_requires=["Pillow", "selenium"],
+    packages=find_packages(exclude=("tests",)),
+
+)
