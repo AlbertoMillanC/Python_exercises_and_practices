e54ba27230563d853f9bfa5b5dc19ba209b27143
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-11-20 15:56:26 -0500

    typo 1

--- api_and_practices/server_python/server_first.py
+++ api_and_practices/server_python/server_first.py
@@ -1,11 +1,11 @@
 from wsgiref.simple_server import make_server
 
-def application(env,start_reponse):
+def application(env, start_response):
     headers = [('Content-Type', 'text/plain')]
     
-    start_reponse = ('200 OK', headers)
+    start_response('200 OK', headers)
     
-    return ['Hello World!' .encode('utf-8')]
+    return ['Hello World!'.encode('utf-8')]
 
-server = make_server('localhost',8000, application)
+server = make_server('localhost', 8000, application)
 server.serve_forever()
