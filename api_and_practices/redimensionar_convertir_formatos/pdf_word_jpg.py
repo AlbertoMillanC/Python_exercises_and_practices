a2e4016080173674dabbf9882fd8eae5df3387b3
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-02-02 10:27:25 -0500

    pdf a docx

---
+++ api_and_practices/redimensionar_convertir_formatos/pdf_word_jpg.py
@@ -0,0 +1,14 @@
+import os
+import docx
+
+folder = r"C:\Users\ASUS\Downloads\Photos"
+
+for filename in os.listdir(folder):
+    if filename.endswith(".pdf"):
+        pdf_path = os.path.join(folder, filename)
+        docx_filename = filename.split(".pdf")[0] + ".docx"
+        docx_path = os.path.join(folder, docx_filename)
+        
+        doc = docx.Document()
+        doc.add_page_break()
+        doc.save(docx_path)
