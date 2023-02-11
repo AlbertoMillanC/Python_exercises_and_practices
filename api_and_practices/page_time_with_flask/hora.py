e836d618be409093234282120de7078fca83e18d
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-02-11 18:47:35 -0500

    add /tamplates

--- api_and_practices/page_time_with_flask/hora.py
+++ api_and_practices/page_time_with_flask/hora.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "La hora actual es: " + str(datetime.now().time())
+    hora_actual = str(datetime.now().time())
+    return render_template("index.html", hora=hora_actual)
 
 if __name__ == "__main__":
     app.run()
