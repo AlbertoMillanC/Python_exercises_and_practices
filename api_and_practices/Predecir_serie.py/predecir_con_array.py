a0e4f47c783c07bf45451646c85e1c01bcab1aba
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-02-16 08:30:07 -0500

    add file predecir cambio de parametros

--- api_and_practices/Predecir_serie.py/predecir_con_array.py
+++ api_and_practices/Predecir_serie.py/predecir_con_array.py
@@ -3,23 +3,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 # Crear un array con 40 números para entrenamiento y otro con 20 para prueba
-numeros = [i for i in range(1, 61)]
-numeros_entrenamiento = numeros[:40]
-numeros_prueba = numeros[40:]
+numeros = [i for i in range(000000, 999999) if len(set(str(i))) == 4]
+numeros_entrenamiento = numeros[:9999]
+numeros_prueba = numeros[2000:]
 
 # Crear un DataFrame con los datos de los números
 datos = pd.DataFrame({'Numero': numeros_entrenamiento})
 
 # Añadir columnas para representar los 6 últimos números ganadores
-for i in range(1, 7):
+for i in range(0, 6):
     datos[f'Ganador{i}'] = datos['Numero'].shift(i)
 
-# Eliminar las filas que contienen valores nulos
+# Eliminar las filas quecontienen valores nulos
 datos = datos.dropna()
 
 # Dividir los datos en conjuntos de entrenamiento y prueba
 x_entrenamiento, x_prueba, y_entrenamiento, y_prueba = train_test_split(
-    datos.iloc[:, :-1], datos.iloc[:, -1], test_size=0.33, random_state=0)
+    datos.iloc[:, :-1], datos.iloc[:, -1], test_size=0.35, random_state=0)
 
 # Crear y ajustar el modelo de regresión lineal
 modelo = LinearRegression()
