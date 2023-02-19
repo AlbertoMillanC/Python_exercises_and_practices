610860f354a42ed5e89a38215f867d988034920b
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-02-18 21:04:12 -0500

    include definicion entrada como variable

--- api_and_practices/Predecir_serie.py/predecir_redes_neuronales/predecir_proceso_por_lotes.py
+++ api_and_practices/Predecir_serie.py/predecir_redes_neuronales/predecir_proceso_por_lotes.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from sklearn.model_selection import train_test_split, KFold
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 
@@ -8,7 +9,8 @@ with open('api_and_practices/Predecir_serie.py/predecir_redes_neuronales/numeros
     data = json.load(f)
     
 print(data)
-    
+
+
 # Preprocesar los datos
 numeros = [int(x) for x in data['numeros']]
 secuencia_longitud = 10  # la longitud de la secuencia de entrada
@@ -20,11 +22,17 @@ for i in range(len(numeros) - secuencia_longitud):
     X.append(secuencia)
     y.append(objetivo)
     
+entrada = np.array([numeros[-secuencia_longitud:]])
+entrada = entrada / float(len(set(numeros)))
+    
 # Convertir los datos a matrices numpy
 X = np.reshape(X, (len(X), secuencia_longitud, 1))
 X = X / float(len(set(numeros)))
 y = np.array(y)
 y = y / float(len(set(numeros)))
+
+# Dividir los datos en conjuntos de entrenamiento y validación
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
 
 # Crear el modelo de RNN
 modelo = Sequential()
@@ -34,16 +42,28 @@ modelo.add(Dense(1, activation='linear'))
 # Compilar el modelo
 modelo.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-# Entrenar el modelo
-modelo.fit(X, y, epochs=100, batch_size=1, verbose=2)
+# Definir el número de lotes y la cantidad de épocas por lote
+batch_size = 32
+epochs = 100
 
-# Evaluar el modelo
-puntuacion = modelo.evaluate(X, y, verbose=0)
-print('Precisión del modelo: %.2f%%' % (puntuacion[1]*100))
+# Entrenar el modelo por lotes y validar en el conjunto de validación
+kf = KFold(n_splits=X_train.shape[0] // batch_size)
+for train_idx, val_idx in kf.split(X_train):
+    X_train_batch, y_train_batch = X_train[train_idx], y_train[train_idx]
+    X_val_batch, y_val_batch = X_train[val_idx], y_train[val_idx]
+    modelo.fit(X_train_batch, y_train_batch, batch_size=batch_size, epochs=epochs, verbose=2)
+    puntuacion = modelo.evaluate(X_val_batch, y_val_batch, verbose=0)
+    print('Precisión del modelo en validación: %.2f%%' % (puntuacion[1]*100))
+
+# Evaluar el modelo en el conjunto de prueba
+puntuacion = modelo.evaluate(X_val, y_val, verbose=0)
+print('Precisión del modelo en prueba: %.2f%%' % (puntuacion[1]*100))
 
 # Hacer una predicción
-entrada = np.array([numeros[-secuencia_longitud:]])
 entrada = entrada / float(len(set(numeros)))
 prediccion = modelo.predict(entrada, verbose=0)
 prediccion = prediccion * float(len(set(numeros)))
 print('Predicción: %d' % prediccion)
+
+
+
