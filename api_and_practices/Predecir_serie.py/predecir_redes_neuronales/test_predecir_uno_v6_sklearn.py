b59bd664f0ef45e89465f565294eb7789a8cd399
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-02-19 15:23:37 -0500

    add file entrenado modelo test dos ( aun no funciona)

--- api_and_practices/Predecir_serie.py/predecir_redes_neuronales/test_predecir_uno_v6_sklearn.py
+++ api_and_practices/Predecir_serie.py/predecir_redes_neuronales/test_predecir_uno_v6_sklearn.py
@@ -14,12 +14,14 @@ X = df[['Num1', 'Num2', 'Num3', 'Num4']]
 y = df['Jackpot']
 
 # Crear un modelo de red neuronal y ajustarlo a los datos
-model = MLPRegressor(hidden_layer_sizes=(10,10,10), max_iter=1000)
+model = MLPRegressor(hidden_layer_sizes=(20, 5,20, 5,20, 5,20, 5,20, 5,20, 5,20, 5,20, 5,20, 5,20, 5,20, 5,20, 5,20, 5, 10, 10, 10), max_iter=100000000)
 model.fit(X, y)
 
 # Predecir el valor del jackpot para un nuevo conjunto de números ganadores
-nuevos_numeros = [[8, 9, 4, 2]]
+nuevos_numeros = [[9, 2, 0, 0]]
 prediccion = model.predict(nuevos_numeros)
-prediccion = np.clip(prediccion, 0, 9999)
+prediccion = np.clip(prediccion, 0000, 9999)
+
+
 
 print("La predicción para los números ganadores es:", int(prediccion))
