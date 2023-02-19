0595b4ea85613f5317773cff58e4200d79b4d57b
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-02-18 21:12:10 -0500

    add file version 1 de test prueba cambiando linear por sigmoide

---
+++ api_and_practices/Predecir_serie.py/predecir_redes_neuronales/test_predecir_uno_v1_sigmoide.py
@@ -0,0 +1,50 @@
+import json
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.activations import sigmoid
+
+# Cargar datos del archivo JSON
+with open('api_and_practices/Predecir_serie.py/predecir_redes_neuronales/numeros_ganadores.json') as f:
+    data = json.load(f)
+    
+print(data)
+    
+# Preprocesar los datos
+numeros = [int(x) for x in data['numeros']]
+secuencia_longitud = 10  # la longitud de la secuencia de entrada
+X = []
+y = []
+for i in range(len(numeros) - secuencia_longitud):
+    secuencia = numeros[i:i+secuencia_longitud]
+    objetivo = numeros[i+secuencia_longitud]
+    X.append(secuencia)
+    y.append(objetivo)
+    
+# Convertir los datos a matrices numpy
+X = np.reshape(X, (len(X), secuencia_longitud, 1))
+X = X / float(len(set(numeros)))
+y = np.array(y)
+y = y / float(len(set(numeros)))
+
+# Crear el modelo de RNN
+modelo = Sequential()
+modelo.add(LSTM(32, input_shape=(X.shape[1], X.shape[2])))
+modelo.add(Dense(1, activation=sigmoid))
+
+# Compilar el modelo
+modelo.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+
+# Entrenar el modelo
+modelo.fit(X, y, epochs=100, batch_size=1, verbose=2)
+
+# Evaluar el modelo
+puntuacion = modelo.evaluate(X, y, verbose=0)
+print('Precisión del modelo: %.2f%%' % (puntuacion[1]*100))
+
+# Hacer una predicción
+entrada = np.array([numeros[-secuencia_longitud:]])
+entrada = entrada / float(len(set(numeros)))
+prediccion = modelo.predict(entrada, verbose=0)
+prediccion = prediccion * float(len(set(numeros)))
+print('Predicción: %d' % prediccion)
