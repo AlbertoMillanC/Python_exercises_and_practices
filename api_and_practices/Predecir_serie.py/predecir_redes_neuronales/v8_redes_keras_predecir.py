eb923146940bb068b60f4322f9190a6441c0b0cb
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-02-20 09:40:36 -0500

    add file tenserflow v8 funciona pero no predice

--- api_and_practices/Predecir_serie.py/predecir_redes_neuronales/v8_redes_keras_predecir.py
+++ api_and_practices/Predecir_serie.py/predecir_redes_neuronales/v8_redes_keras_predecir.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from datetime import datetime
 from tensorflow.keras.models import load_model
 
-modelo = load_model('modelo_entrenado.h5')
+modelo = load_model('modelo_entrenado8.h5')
 
 def guardar_prediccion(prediccion):
     fecha_hora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -15,7 +15,7 @@ def guardar_prediccion(prediccion):
     except:
         data = {"predicciones": []}
     predicciones = data.get('predicciones', [])
-    predicciones.append({'fecha_hora': fecha_hora, 'prediccion v7': prediccion_str})
+    predicciones.append({'fecha_hora': fecha_hora, 'prediccion v8': prediccion_str})
     with open('numerosV7.json', 'w') as f:
         json.dump({'predicciones': predicciones}, f, indent=4)
 
@@ -25,7 +25,7 @@ with open('api_and_practices/Predecir_serie.py/predecir_redes_neuronales/numeros
 
 # Preprocesar los datos
 numeros = [int(x) for x in data['numeros']]
-secuencia_longitud = 60  # la longitud de la secuencia de entrada
+secuencia_longitud = 40  # la longitud de la secuencia de entrada
 X = []
 y = []
 for i in range(len(numeros) - secuencia_longitud):
@@ -42,23 +42,18 @@ y = y / float(9999)
 
 # Crear el modelo de RNN
 modelo = tf.keras.models.Sequential([
-    tf.keras.layers.LSTM(32, input_shape=(X.shape[1], X.shape[2])),
+    tf.keras.layers.LSTM(64, input_shape=(X.shape[1], X.shape[2])),
     tf.keras.layers.Dense(1, activation='linear')
 ])
 
 # Compilar el modelo
-modelo.compile(loss='mean_absolute_error', optimizer='adam', metrics=['acc'])
+modelo.compile(loss='huber_loss', optimizer='adam', metrics=['acc'])
 
 # Entrenar el modelo
-modelo.fit(X, y, epochs=100, batch_size=100, verbose=3)
+modelo.fit(X, y, epochs=1000, batch_size=1000, verbose=2)
 
-# Hacer una predicción
-entrada = np.array([numeros[-secuencia_longitud:]])
-entrada = entrada / float(9999)
-prediccion = modelo.predict(entrada, verbose=0)
-prediccion = prediccion * 9999
-
-# Repetir la predicción hasta que sea igual a 9200
+# Hacer diez predicciones y guardarlas
+predicciones = []
 for i in range(100):
     entrada = np.array([numeros[-secuencia_longitud:]])
     entrada = entrada / float(9999)
@@ -66,10 +61,9 @@ for i in range(100):
     prediccion = prediccion * 9999
     if round(prediccion[0][0]) == 9200:
         break
-
-
-# Guardar los parámetros del modelo en un archivo
-modelo.save('modelo_entrenado.h5')
+    predicciones.append(prediccion)
+    
+modelo.save('modelo_entrenado8.h5')
 
 print('Predicción: %d' % prediccion)
 
