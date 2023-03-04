4ccb072383ac14330ea02f6d887818873556370c
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-03-04 01:42:32 -0500

    add file test 8 save content html for each page

--- api_and_practices_(index_A)/(index_16)funciones_scraping/selenium scraping/test_8_selenium_dropdown.py
+++ api_and_practices_(index_A)/(index_16)funciones_scraping/selenium scraping/test_8_selenium_dropdown.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -9,3 +8,12 @@ driver.get("https://www.colombialoterias.com/loteria-de-boyaca/")
 select = Select(driver.find_element(By.CSS_SELECTOR, "#rrm-draws-datetime-list"))
 for option in select.options:
     print(option.text)
+    
+    # Obtener el contenido HTML de la página
+    html_content = driver.find_element(By.CSS_SELECTOR, "body").get_attribute("innerHTML")
+    
+    # Escribir el contenido HTML en un archivo de texto
+    with open(f"{option.text}.txt", "w") as f:
+        f.write(html_content)
+
+driver.quit()
