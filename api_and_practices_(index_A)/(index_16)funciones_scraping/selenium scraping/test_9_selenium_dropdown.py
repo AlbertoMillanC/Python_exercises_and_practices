2ca523a9e1bcaf7ad56860a44297f1386b264972
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-03-04 02:00:20 -0500

    add file test 9  explore content class 'resultados' in each page

---
+++ api_and_practices_(index_A)/(index_16)funciones_scraping/selenium scraping/test_9_selenium_dropdown.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome()
+driver.get("https://www.colombialoterias.com/loteria-de-boyaca/")
+
+select = Select(driver.find_element(By.CSS_SELECTOR, "#rrm-draws-datetime-list"))
+for option in select.options:
+    print(option.text)
+    
+    # Obtener el contenido HTML de la página
+    html_content = driver.find_element(By.CLASS_NAME, "rrm-draw-numbers").get_attribute("innerHTML")
+    
+    # Escribir el contenido HTML en un archivo de texto
+    with open(f"{option.text}.txt", "w") as f:
+        f.write(html_content)
+
+driver.quit()
