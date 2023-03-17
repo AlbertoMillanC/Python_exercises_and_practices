3edb80d8c60c390a1c9c1b2eb678a51fc46411ba
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-03-16 23:37:59 -0500

    incex replace names and order numbers

--- api_and_practices_(index_A)/(index_17)_Id_and_classifying_images/add_files_blob_azure.py
+++ api_and_practices_(index_A)/(index_17)_Id_and_classifying_images/add_files_blob_azure.py
@@ -13,8 +13,17 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Iniciamos la captura de video
 cap = cv2.VideoCapture(0)
 
-# Inicializamos el contador de rostros
+# Inicializamos el contador de rostros y la lista de rostros detectados
 face_counter = 0
+detected_faces = []
+
+def is_new_face(faces):
+    # Compara las caras detectadas con las caras previamente almacenadas
+    for face in faces:
+        for detected_face in detected_faces:
+            if np.allclose(face, detected_face, atol=20):
+                return False
+    return True
 
 while True:
     # Leemos un fotograma del video
@@ -27,4 +36,32 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     # Dibujamos un rectángulo alrededor de cada rostro y asignamos un código
-    for (x
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        face_counter += 1
+
+    # Verificamos si es un rostro nuevo y lo almacenamos en la lista de rostros detectados
+    if is_new_face(faces):
+        detected_faces.extend(faces)
+
+        # Convertir la imagen a un objeto de memoria
+        _, img_encoded = cv2.imencode('.jpg', frame)
+        img_bytes = img_encoded.tobytes()
+
+        # Crear un nombre de archivo único para el blob
+        blob_name = "rostro_{}.jpg".format(face_counter)
+
+        # Cargar el blob en Azure Storage
+        blob_client = container_client.get_blob_client(blob_name)
+        blob_client.upload_blob(img_bytes, overwrite=True)
+
+    # Mostramos el fotograma resultante
+    cv2.imshow('frame',frame)
+
+    # Salimos del bucle si se presiona la tecla 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberamos los recursos al terminar
+cap.release()
+cv2.destroyAllWindows()
